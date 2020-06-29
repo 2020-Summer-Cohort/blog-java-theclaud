2b1fd6cdaedcf26ba7289558bfdcc85c071f05ca
20Claudia20 <leiaofalderran@gmail.com>
2020-06-28 21:20:51 -0400

    working on controllers and Storages

---
+++ src/main/java/org/wcci/blog/BlogPostController.java
@@ -0,0 +1,17 @@
+package org.wcci.blog;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BlogPostController {
+
+//    private BlogPostStorage blogPostStorage;
+//    private HashtagsStorage hashtagsStorage;
+//
+//    public BlogPostController(BlogPostStorage blogPostStorage, HashtagsStorage hashtagsStorage) {
+//        this.blogPostStorage = blogPostStorage;
+//        this.hashtagsStorage = hashtagsStorage;
+//    }
+
+}
---
+++ src/main/java/org/wcci/blog/BlogPostStorage.java
@@ -0,0 +1,7 @@
+package org.wcci.blog;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogPostStorage {
+}
