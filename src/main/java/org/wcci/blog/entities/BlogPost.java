def7e0b4a8f792bfa560858c4121a7a7473b1493
20Claudia20 <leiaofalderran@gmail.com>
2020-07-21 18:26:21 -0400

    picking up where I left off to finalize this project, edited Pojos

--- src/main/java/org/wcci/blog/entities/BlogPost.java
+++ src/main/java/org/wcci/blog/entities/BlogPost.java
@@ -1,23 +1,17 @@
 package org.wcci.blog.entities;
 
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
-@Entity
+
 public class BlogPost {
 
-    @Id
-    @Entity
+
     private String postTitle;
     private String postDescription;
     private String author;
     private String postDate;
-    @ManyToOne
     private Collection<Hashtags> hashtags;
 
 
