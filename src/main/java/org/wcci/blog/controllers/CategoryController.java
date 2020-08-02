bc3a0f19267d42c521b3c344a3ff1af3148da533
20Claudia20 <leiaofalderran@gmail.com>
2020-08-02 14:26:05 -0400

    cleaning up category storage, noticed the template was missing thyme going back to edit

--- src/main/java/org/wcci/blog/controllers/CategoryController.java
+++ src/main/java/org/wcci/blog/controllers/CategoryController.java
@@ -1,16 +1,29 @@
 package org.wcci.blog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.wcci.blog.entities.BlogPost;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.wcci.blog.storage.CategoryStorage;
 
 @Controller
 public class CategoryController {
 
-    Map<String, BlogPost> posts = new HashMap<>();
+   private CategoryStorage categoryStorage;
 
+   public CategoryController(CategoryStorage categoryStorage){
+       this.categoryStorage=categoryStorage;
+   }
 
+    @RequestMapping("category/{categoryTitle}")
+    public String showSingleCategory(@PathVariable String categoryTitle, Model model) {
+       model.addAttribute("category", categoryStorage.findCategoryByTitle(categoryTitle));
+       return "category-template";
+    }
 
+    @RequestMapping("category")
+    public String showAllCategories(Model model){
+       model.addAttribute("category", categoryStorage.findAllCategories());
+       return "category-template";
+    }
 }
--- src/main/java/org/wcci/blog/storage/CategoryStorage.java
+++ src/main/java/org/wcci/blog/storage/CategoryStorage.java
@@ -2,14 +2,21 @@ package org.wcci.blog.storage;
 
 import org.wcci.blog.entities.Category;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CategoryStorage {
 
     private Map<String, Category> categories = new HashMap<>();
-        categories.put("Top Dwelling Fish", new Category("Top Dwellers", "desc",null));
 
+//        categories.put("Top Dwelling Fish", new Category("Top Dwellers", "desc",null)
 
+    public Object findCategoryByTitle(String categoryTitle) {
+        return categories.get(categoryTitle);
+    }
 
+    public Collection<Category> findAllCategories() {
+        return categories.values();
+    }
 }
