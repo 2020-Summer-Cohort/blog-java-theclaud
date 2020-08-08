ce6eba3307a94b7a1cf9ff19887454b396ce9406
20Claudia20 <leiaofalderran@gmail.com>
2020-08-08 01:52:28 -0400

    editing blog storage constructor and fields

--- src/main/java/org/wcci/blog/storage/BlogPostStorage.java
+++ src/main/java/org/wcci/blog/storage/BlogPostStorage.java
@@ -4,30 +4,34 @@ import org.springframework.stereotype.Service;
 import org.wcci.blog.storage.repository.BlogPostRepository;
 import org.wcci.blog.entities.BlogPost;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class BlogPostStorage {
 
-    Map<String, BlogPost> posts = new HashMap<>();
+//    Map<String, BlogPost> posts = new HashMap<>();
 
-    BookRe
+    BlogPostRepository blogPostRepo;
 
-    public BlogPostStorage () {
-        posts.put("Angel Fish", new BlogPost("Angel Fish", "Angel Fish are graceful when they're swimming around.I enjoy watching\n" +
+    public BlogPostStorage(BlogPostRepository blogPostRepo) {
+        this.blogPostRepo = blogPostRepo;
+
+
+
+        BlogPost post1 = new BlogPost("Angel Fish", new BlogPost("Angel Fish", "Angel Fish are graceful when they're swimming around.I enjoy watching\n" +
                 " them move around and show off their fins, the way they flow with the\n" +
                 " water is magical to see. The Angel fish grows to be 8 inches big and\n" +
                 " they vary in pattern The Marbled Angel has a mix of black,blue and\n" +
                 " gray. A classic must have for your growing aquarium.", "Leia Alderran", "06/26/20", hashtag1, hashtag2));
 
-        posts.put("Kamaka Fish", new BlogPost("Kamaka Fish", "The Kamaka Rainbow fish are known for their impeccable, colorful\n" +
+        BlogPost post2 = new BlogPost("Kamaka Fish", new BlogPost("Kamaka Fish", "The Kamaka Rainbow fish are known for their impeccable, colorful\n" +
                 " pattern on it's scales. With many hints of blue, from sky blue to\n" +
                 " turquoise, it's blinding colors will leave you in awe. I love watching\n" +
                 " them swim around during golden hour when their vibrant colors show off\n" +
                 " the most. Preferably kept in schools with other rainbow fish, in warm\n" +
-                " temperature water.", "Claudia Barrales", "6/26/20", hashtag1, hashtag2));
+                " temperature water.", "Claudia Barrales", "6/26/20", Hashtags...hashtags));
+
+        blogPost.put(post1.getPostTitle(), post1);
     }
+
 
 
     public BlogPost findBlogPostByPostTitle(String postTitle) {
@@ -39,16 +43,5 @@ public class BlogPostStorage {
     }
 }
 
-//        return postRepo.findByPostTitle(postTitle);
-
-//    public BlogPostStorage(BlogPostRepository postRepo) {
-//        this.postRepo = postRepo;
-//    }
-//
-
-//
-//    public Iterable<BlogPost> findAllPosts() {
-//        return postRepo.findAll();
-//    }
 
 
